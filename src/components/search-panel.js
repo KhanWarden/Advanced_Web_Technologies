c31bd90510480255aa81872b57c414501f169e91
KhanWarden <baryrhan2004@gmail.com>
2025-01-29 11:27:46 +0000

    Step 6: Creating and formatting Project Structure

---
+++ src/components/search-panel.js
@@ -0,0 +1,6 @@
+import React from "react";
+const SearchPanel = () => {
+    return <input placeholder= "searchText" />;
+};
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-const TodoList = () => {
-    const items = ["Install React", "Study React", "Use React", "Build React App"]
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-            <li>{items[2]}</li>
-            <li>{items[3]}</li>
-        </ul>
-    );
-};
-
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-    const searchText = "Type here to search";
-    const searchStyle = {
-        fontSize: "20px",
-    };
-
-    return <input
-        style={searchStyle}
-        placeholder={searchText}
-        disabled={true} />;
-};
+import ReactDOM from 'react-dom/client';
+import AppHeader from './components/app-header';
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
 const App = () => {
-    const value = "<script>alert (\"\")</script>";
-    // const isLoggedIn = false;
-    // const loginBox = <span>Log in to the system</span>;
-    // const welcomeBox = <span>Welcome Back</span>;
-
     return (
         <div>
-            { value }
-            {/*{ isLoggedIn ? null : loginBox }*/}
             <AppHeader />
             <SearchPanel />
             <TodoList />
@@ -46,4 +14,5 @@ const App = () => {
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
